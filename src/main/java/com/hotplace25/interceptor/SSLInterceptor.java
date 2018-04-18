085fa5915fe6a6403863d666cc3af575020ad7a1
Your Name <you@example.com>
2018-04-18 11:23:30 +0900

    ssl

---
+++ src/main/java/com/hotplace25/interceptor/SSLInterceptor.java
@@ -0,0 +1,26 @@
+package com.hotplace25.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class SSLInterceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		String url = request.getRequestURL().toString();
+		
+		if(url.startsWith("http://")) {
+			url = url.replace("http://", "https://");
+			response.sendRedirect(url);
+			return false;
+		}
+		
+		return true;
+	}
+	
+	
+}
