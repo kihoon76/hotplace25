c19607e4c70320e4e38399c277e6dd9f1d7fd736
Your Name <you@example.com>
2018-05-09 15:59:29 +0900

    qna

--- src/main/java/com/hotplace25/interceptor/SSLInterceptor.java
+++ src/main/java/com/hotplace25/interceptor/SSLInterceptor.java
@@ -3,10 +3,14 @@ package com.hotplace25.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-public class SSLInterceptor extends HandlerInterceptorAdapter {
+import com.hotplace25.util.HttpHeaderUtil;
 
+public class SSLInterceptor extends HandlerInterceptorAdapter {
+	private static final Logger logger = LoggerFactory.getLogger(SSLInterceptor.class);
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
@@ -17,7 +21,7 @@ public class SSLInterceptor extends HandlerInterceptorAdapter {
 			url = url.replace("http://", "https://");
 		}
 		
-		System.out.println("url ===> " + url);
+		logger.debug("url ===> " + url);
 		if(url.startsWith("https://hotplace25.com")) {
 			url = url.replace("https://hotplace25.com", "https://www.hotplace25.com");
 			response.sendRedirect(url);
