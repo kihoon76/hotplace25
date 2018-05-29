29df6ed75b2cc67aa00b618dbc553b0f34221e8d
Ki Hoon <lovedeer118@naver.com>
2018-05-29 13:36:51 +0900

    1

--- src/main/resources/META-INF/resources/js/src/map/hotplace.calc.js
+++ src/main/resources/META-INF/resources/js/src/map/hotplace.calc.js
@@ -110,7 +110,7 @@
 			calc.sujibunseog.calcIncomeManageImdae();		//운영 > 임대
 			
 			//다른 모든 초기화가 끝나고 양도세를 세팅한다
-			calc.sujibunseog.calcYangdose(true);
+			
 		}
 		
 		/**
@@ -596,7 +596,7 @@
 				//채권매입비
 				hotplace.calc.sujibunseog.calcPurchaseChaegwon(true);
 				//양도세
-				//hotplace.calc.sujibunseog.calcYangdose(true);
+				hotplace.calc.sujibunseog.calcYangdose(true);
 				//농지보전부담금
 				hotplace.calc.sujibunseog.calcFarmBudam(suji.isFarmBudamGammyeon());
 				//대체산림자원조성비
@@ -803,17 +803,11 @@
 				var $WYangdose = $(suji.getWYangdoseId());
 				
 				if(isSet) {
-					//매각
-					var $WIncomeSell = $(suji.getWIncomeSellId());
-					var $WJichool = $(suji.getWJichoolId());
-					
-					//var $WPurchase = $(suji.getWPurchaseId());
-					var _$$1 = $WIncomeSell.data('value') - $WJichool.data('value');//$WPurchase.data('value');
-					_$$1 = Math.round(_$$1);
+					var $WPurchase = $(suji.getWPurchaseId());
+					var _$$1 = $WPurchase.data('value');
 					
 					$stepYangdose.data('value', _$$1);
-					//$stepYangdose.val($WPurchase.val() + $stepYangdose.data('suffix'));
-					$stepYangdose.val(_$$1.toString().money() + $stepYangdose.data('suffix'));
+					$stepYangdose.val($WPurchase.val() + $stepYangdose.data('suffix'));
 					
 					$stepYangdose.data('step', makeStep(_$$1, hotplace.config.yangdoseStepPercent));
 				}
