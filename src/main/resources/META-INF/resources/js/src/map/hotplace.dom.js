09cf59e04d5bfc51922d5a1456e467d9d84203db
Ki Hoon <lovedeer118@naver.com>
2018-06-15 15:37:43 +0900

    1

--- src/main/resources/META-INF/resources/js/src/map/hotplace.dom.js
+++ src/main/resources/META-INF/resources/js/src/map/hotplace.dom.js
@@ -644,6 +644,7 @@
 			.tabs()
 			.addClass('ui-tabs-vertical ui-helper-clearfix');
 			
+			//https://developers.google.com/youtube/player_parameters?hl=ko
 			$('#tuto1').attr('src', 'https://www.youtube.com/embed/7RdSsO-AMmc?vq=hd1080&rel=0');
 			$('#tuto2').attr('src', 'https://www.youtube.com/embed/Vop21AvrMP0?vq=hd1080');
 			$('#tuto3').attr('src', 'https://www.youtube.com/embed/cLFH1UOo4z0?vq=hd1080');
