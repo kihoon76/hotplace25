5aa47b24d42c6d28bf4766da9f4dd267a07fec70
Ki Hoon <lovedeer118@naver.com>
2018-07-05 14:53:57 +0900

    1

--- src/main/resources/META-INF/resources/js/src/main.js
+++ src/main/resources/META-INF/resources/js/src/main.js
@@ -248,7 +248,10 @@ $(document).ready(function() {
 	$('#myCurrentPosition').on('click', function() {
 		if('geolocation' in navigator) {
 			// 지오로케이션 사용 가능 
+			
+			
 			navigator.geolocation.getCurrentPosition(function(position) {
+				hotplace.dom.showMask();
 				window.location.href = $('body').data('url') + 'main?currentX=' + position.coords.longitude + '&currentY=' + position.coords.latitude;
 			}, function(err) {
 				hotplace.dom.showAlertMsg(null, '위치차단 되었습니다.', hotplace.ALERT_SIZE);
